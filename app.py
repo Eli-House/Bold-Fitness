0e5ae2c969eb55e4cd743ff53ea9963fe86aac4f
Eli-House <ethousenbold@gmail.com>
2023-05-01 10:03:02 -0700

    Created the data base

--- app.py
+++ app.py
@@ -5,9 +5,40 @@ from models import *
 app = Flask(__name__)
 
 # TODO: Change the secret key
-app.secret_key = "Change Me"
+app.secret_key = "JCAilBNTz6CWp2v3ZQ=="
 
 # TODO: Fill in methods and routes
+@app.route("/",methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if password == "123":
+            session["username"] = username
+            return redirect(url_for("profile"))
+        else:
+            flash("Either password or username is incorrect", "error")
+            return render_template("login.html")
+
+@app.route("/exercise")
+def exercise():
+    return render_template("exercise.html")
+    newexercise = Exercise(uname, passOne)
+    db_session.add(newUser)
+    db_session.commit()
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/logout")
+def logout():
+    if "username" in session:
+        session.pop("username")
+    return redirect(url_for("/"))
 
 @app.before_first_request
 def setup():
--- models.py
+++ models.py
@@ -9,4 +9,53 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 # TODO: Complete your models
-    
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column("id", INTEGER, primary_key=True)
+    username = Column("username", TEXT)
+    password = Column("password", TEXT)
+    first_name = Column("first_name", TEXT)
+    last_name = Column("last_name", TEXT)
+    age = Column("age", INTEGER)
+    weight = Column("weight", INTEGER)
+    height = Column("height", TEXT)
+
+    def __init__(self,  username, password, first_name, last_name, age, weight, height):
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+class Workout(Base):
+    __tablename__ = "workouts"
+
+    id = Column("id", INTEGER, primary_key=True)
+    user_id = Column("user_id", ForeignKey("users.id"))
+    lift_id = Column("lift_id", ForeignKey("lifts.id"))
+    num_reps = Column("num_reps", INTEGER)
+
+    def __init__(self, user_id, lift_id, num_reps):
+        self.user_id = user_id
+        self.lift_id = lift_id
+        self.num_reps = num_reps
+
+class Lift(Base):
+    __tablename__ = "lifts"
+
+    id = Column("id", INTEGER, primary_key=True)
+    muscle_group = Column("muscle_group", TEXT)
+    discription = Column("discription", TEXT)
+
+    def __init__(self, muscle_group, discription):
+        self.muscle_group = muscle_group
+        self.discription = discription
+
+
+
+
+
